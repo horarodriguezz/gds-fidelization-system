a0cf38e16aea97591cd656a252a144f12b9f7bae
horarodriguezz <hora.rodriguezz@gmail.com>
2025-11-04 04:14:11 -0300

    extended UserModel to use Sanctum

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -1,15 +1,18 @@
 <?php
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasUuids;
+    use Notifiable;
 
-    protected $fillable = ['name', 'email', 'password', 'phone_number', 'role', 'business_id', 'confirmed'];
+    protected $fillable = ['first_name', 'last_name', 'email', 'password', 'phone_number', 'role', 'business_id', 'confirmed'];
 
     public function business(): BelongsTo {
         return $this->belongsTo(Business::class);
