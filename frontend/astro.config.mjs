85b0e60b41426149ac7dbbb363041149bd4e67ee
horarodriguezz <hora.rodriguezz@gmail.com>
2025-11-17 03:05:17 -0300

    added redirections and UnderConstruction element

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,21 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+// @ts-ignore
+import { Pathname } from "@/config/Pathname";
+import react from "@astrojs/react";
 
-import react from '@astrojs/react';
+import node from "@astrojs/node";
 
-import node from '@astrojs/node';
-
-// https://astro.build/config
+// https://astro.buil d/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
   integrations: [react()],
 
   adapter: node({
-    mode: 'standalone'
-  })
+    mode: "standalone",
+  }),
+
+  redirects: {
+    "/": Pathname.BUSINESS_DASHBOARD,
+  },
 });
